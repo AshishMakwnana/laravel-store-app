ea15c4c776f57e5466a9fbd23ff696c55f168b8a
unknown <ashishmakwana435@gmail.com>
2025-04-16 23:32:20 +0530

    api task with chat gpt

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,11 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function apiTest()
+    {
+        return response()->json([
+            'message' => 'API test successful',
+            'status' => 200,
+        ]);
+    }
 }
---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('api-test',Controller::class.'@apiTest');
