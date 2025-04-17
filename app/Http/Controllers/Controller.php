4985182c181e4dff2cc50e2d02a32d3dea503959
unknown <ashishmakwana435@gmail.com>
2025-04-17 08:09:24 +0530

    in api expect name perameter and return a message

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Http\Request as HttpRequest;
+use Illuminate\Support\Facades\Validator;
+
 abstract class Controller
 {
-    public function apiTest()
+    public function apiTest(HttpRequest $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors(),
+                'status' => 422,
+            ], 422);
+        }
         return response()->json([
-            'message' => 'API test successful',
+            'message' => "hello $request->name",
             'status' => 200,
         ]);
     }
