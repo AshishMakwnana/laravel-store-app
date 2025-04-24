0904272a004317d632620ebabcfaa09bf24084bc
Ashish Makwana <118716252+AshishMakwnana@users.noreply.github.com>
2025-04-24 17:01:04 +0530

    cart opration done

--- app/Http/Controllers/CartItemController.php
+++ app/Http/Controllers/CartItemController.php
@@ -6,7 +6,12 @@ use App\ApiResponseTrait;
 use App\Http\Requests\CartRequest;
 use App\Models\CartItem;
 use Exception;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class CartItemController extends Controller
 {
@@ -16,56 +21,51 @@ class CartItemController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $items = auth()->user()->cartItems()->with('product')->get();
+            return $this->successResponse($items);
+        } catch (Exception $e) {
+           return $this->errorResponse('Failed to fetch cart items', 500, $e->getMessage());
+        }
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
     /**
      * Store a newly created resource in storage.
      */
     public function store(CartRequest $request)
     {
-        try{
-            $cartItem = CartItem::create([
-                'user_id' => auth()->user()->id,
-                'product_id' => $request->product_id,
-                'quantity' => $request->quantity
-            ]);
-            return $this->successResponse($cartItem,'Item added to cart successfully');
-        }catch(Exception $e){
-            $this->errorResponse('Failed to add item to cart',500,$e->getMessage());
+        try {
+            $cartItem = CartItem::updateOrCreate(
+                ['user_id' => auth()->id(), 'product_id' => $request->product_id],
+                ['quantity' => DB::raw("quantity + {$request->quantity}")]
+            );
+            return $this->successResponse($cartItem, 'Item added to cart successfully');
+        } catch (Exception $e) {
+            $this->errorResponse('Failed to add item to cart', 500, $e->getMessage());
         }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(CartItem $cartItem)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(CartItem $cartItem)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, CartItem $cartItem)
+    public function update(Request $request,$id)
     {
-        //
+        try{
+           $validator = Validator::make($request->all(),[
+                'quantity' => 'required|integer|min:1'
+            ]);
+            if($validator->fails()){
+                return $this->errorResponse('Validation error',422,$validator->errors());
+            }
+            $item = CartItem::where('id',$id)->where('user_id',auth()->id())->firstOrFail();
+            $item->update($validator->validated());
+            return $this->successResponse($item,'cart item updated succssfully',200);
+        }catch(ModelNotFoundException $e){
+            return $this->errorResponse('Cart Item Not Found',404,$e->getMessage());
+        }catch(Exception $e){
+            return $this->errorResponse('server error',500,$e->getMessage());
+        }
     }
 
     /**
@@ -73,6 +73,11 @@ class CartItemController extends Controller
      */
     public function destroy(CartItem $cartItem)
     {
-        //
+        try{
+            $cartItem->delete();
+            return $this->successResponse('Item remove form the cart');
+        }catch(Exception $e){
+            return $this->errorResponse();
+        }
     }
 }
